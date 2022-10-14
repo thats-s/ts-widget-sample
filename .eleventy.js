67bbf090dcf336af9422ee7e4d4e3ed145293ce8
MLC 湯浅峻 <yuasa-shun@mitsue.co.jp>
2022-10-14 15:05:44 +0900

    remove: eleventy

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(eleventyConfig) {
-    return {
-        dir: {
-            input: 'src/ejs',
-            output: 'public',
-        },
-        htmlTemplateEngine: 'ejs'
-    };
-};
