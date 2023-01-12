bba68ad2d392d51239797d0a832b1bf5feabf2ee
thats-s <rudy5shun@gmail.com>
2023-01-12 19:49:38 +0900

    feat: addon追加（@whitespace/storybook-addon-html）

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,8 @@ module.exports = {
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
-        "@storybook/addon-storysource"
+        "@storybook/addon-storysource",
+        "@whitespace/storybook-addon-html"
     ],
     "framework": "@storybook/html"
 }
